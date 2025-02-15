a9e7e3f6f96bc42805b56c7f195ae24b0ef0f15e
JB Reefer <jb@jbreefer.nyc>
2025-02-15 14:08:49 -0500

    Description

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,6 +6,7 @@ export async function GET(context) {
 	const posts = await getCollection("news");
 	return rss({
 		title: SITE_TITLE,
+		description: "Placeholder",
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
