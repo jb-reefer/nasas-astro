dd3b24379e8dddeb47b2b22a645d4181affd3651
JB Reefer <jb@jbreefer.nyc>
2025-12-31 22:01:06 -0500

    Ignore build artifacts

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default [
+	{
+		ignores: ['dist/', 'node_modules/', '.astro/', '.vercel/'],
+	},
 	...eslintPluginAstro.configs.recommended,
 ];
